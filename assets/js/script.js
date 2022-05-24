e0a97929bf85911ca559e49bf2ae8e8b410ca3b0
Gromit <eversoryan@gmail.com>
2022-05-23 20:49:37 -0600

    Changes to HTML

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Variables grabbed from HTML
+
+
+// Variables
+let cityOne = "Austin";
+let CityTwo = "Chicago";
+let cityThree = "New York";
+let cityFour = "Orlando";
+let cityFive = "San Francisco";
+
+// function for getting cities weather
+
+
+// Function for search and city history
